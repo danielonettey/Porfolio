0e7e4b957f83f5363cce2d912693209ef67c9bd2
daniel-nettey <106527087+daniel-nettey@users.noreply.github.com>
2022-08-14 11:18:47 +0000

    Social Design

---
+++ src/components/SocialsSection.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import GithubIcon from '../assets/icons/GitHubIcon.png'
+import LinkedInIcon from '../assets/icons/LinkedinIcon.png'
+import EmailIcon from '../assets/icons/EmailIcon.png'
+
+
+const SocialsSection = () => {
+    const socialLinks = [
+        {
+            image: GithubIcon,
+            url: '',
+        },
+        {
+            image: LinkedInIcon,
+            url: '',
+        },
+        {
+            image: EmailIcon,
+            url: '',
+        },
+    ]
+    return (
+        <>
+            <div className='hidden sm:flex fixed bottom-0 space-y-6 left-10 flex-col items-center'>
+                {
+                    socialLinks.map((socialLink, index) => (
+                        <a href={socialLink.url} key={index} target="_blank" className="cursor-pointer">
+                            <img src={socialLink.image} className=' w-6' />
+                        </a>
+                    ))
+                }
+
+                <div className='h-28 w-px bg-white'></div>
+
+            </div>
+
+            <div className='hidden sm:flex fixed bottom-0 flex-col items-center right-0'>
+                <div className='rotate-90 mb-28 text-sm text-white/80'>
+                    danielonettey@gmail.com
+                </div>  
+                <div className='h-28 w-px bg-white'></div>
+            </div>
+        </>
+    )
+}
+
+export default SocialsSection
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import DynamicText from '../components/DynamicText'
 import Navbar from '../components/Navbar/Navbar'
 import ProjectCard from '../components/ProjectCard'
+import SocialsSection from '../components/SocialsSection'
 import projects from '../data/projects'
 
 
@@ -54,9 +55,9 @@ const HomePage = () => {
 
     return (
         <div className='bg-backColor text-white'>
-            <div className='container mx-auto px-4 relative'>
+            <div className='container mx-auto px-4 '>
                 <Navbar />
-                <div className='max-w-5xl mx-auto'>
+                <div className='max-w-5xl mx-auto relative'>
                     <div className='space-y-5 pt-28 pb-16 sm:py-28'>
                         <p className='text-sm md:text-xl text-secondary font-medium'>Hi, my name is</p>
                         <DynamicText subIndex={22} />
@@ -106,11 +107,7 @@ const HomePage = () => {
 
 
                 {/* Socials Section  */}
-                <div className='fixed bottom-0 '>
-                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-                    <div className='h-28 w-px bg-white'></div>
-
-                </div>
+                <SocialsSection />
             </div>
         </div>
     )
